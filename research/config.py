29c1eb580a0e24dadd5127a013cfb90a7ab5aadf
Dmitriy Karpenko <dmitriy.qua@gmail.com>
2021-08-27 18:09:32 +0300

    first

---
+++ research/config.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+from utils.dataset_utils import get_classes_num
+
+with_pnet_report = False
+with_keras_report = False
+
+with_data_normalization = True
+
+color_mode = "grayscale" # "grayscale" or "rgb"
+
+train_images_path = "images/train"
+test_images_path = "images/test"
+
+train_data_path = 'train_data.csv'
+test_data_path = 'test_data.csv'
+
+model_path = "models"
+
+img_height = 12
+img_width = 12
+
+batch_size = 1
+
+keras_epochs = 2
+pnet_epochs = 2
+
+classes_num = get_classes_num(path=train_images_path)
+	
